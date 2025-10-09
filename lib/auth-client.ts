4468dbcf7d11f115109d25d28f054ee9d8313fd7
jorgefariasso <132616065+jorgefariasso@users.noreply.github.com>
2025-10-09 14:59:26 -0300

    Update auth-client.ts
    comments out

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -5,7 +5,7 @@ import * as SecureStore from "expo-secure-store";
 const baseURL = process.env.EXPO_PUBLIC_API_URL;
 
 export const authClient = createAuthClient({
-    baseURL: baseURL, // Base URL of your Better Auth backend.
+    baseURL: baseURL,
     plugins: [
         expoClient({
             scheme: "tnsnet-tasknet",
